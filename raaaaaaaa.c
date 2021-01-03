3d80cf6392a3c26ec760c652a1f10660b1d177d9
RadiaReza <76839591+RadiaReza@users.noreply.github.com>
2021-01-03 22:57:46 +0600

    Add files via upload

---
+++ raaaaaaaa.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+   int i;
+   for(i=0;i<=3;i++){
+    printf("let me go!\n");
+    if(i==2)
+        break;
+    printf("*\n");
+    printf("**\n");
+   }
+
+
+
+
+
+
+
+
+    return 0;
+}
